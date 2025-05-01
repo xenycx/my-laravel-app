072bfa35cf1f2e6e727579e032d341e9c2d236fb
Aleksandre Dokhnadze <aleqsandredoxnadze@gmail.com>
2025-05-01 22:11:44 +0400

    Updated slide titles and post dates for consistency

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -19,17 +19,17 @@ class MyController extends Controller
     public function slide() {
         $slides = [
             [
-                'title' => 'First Slide',
+                'title' => 'Pirveli',
                 'content' => 'This is the content of the first slide',
                 'image' => 'https://cdn.georgiantravelguide.com/storage/thumbnails/dji-0078-2.jpg'
             ],
             [
-                'title' => 'Second Slide',
+                'title' => 'Meore',
                 'content' => 'This is the content of the second slide',
                 'image' => 'https://cdn.georgiantravelguide.com/storage/files/nikortsminda-racha-nikortsminda-church-racha-1.jpg'
             ],
             [
-                'title' => 'Third Slide',
+                'title' => 'Mesame',
                 'content' => 'This is the content of the third slide',
                 'image' => 'https://georgiavoyage.ge/wp-content/uploads/2025/01/Nikortsminda-cathedral.jpg'
             ]
@@ -40,19 +40,19 @@ class MyController extends Controller
                 'id' => 1,
                 'title' => 'პოსტი პირველი',
                 'content' => 'ეს არის აღწერა პირველი პოსტის',
-                'date' => '2024-03-01'
+                'date' => '2025-04-01'
             ],
             [
                 'id' => 2,
                 'title' => 'მეორე პოსტი',
                 'content' => 'ეს არის აღწერა მეორე პოსტის',
-                'date' => '2024-03-02'
+                'date' => '2025-04-02'
             ],
             [
                 'id' => 3,
                 'title' => 'მესამე პოსტი',
                 'content' => 'ეს არის აღწერა მესამე პოსტის',
-                'date' => '2024-03-03'
+                'date' => '2025-04-03'
             ]
         ];
 
